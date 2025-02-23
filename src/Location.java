eddf168389725234f92b1ec2695a3780e75be291
jonas <jonassekyra@gmail.com>
2025-02-23 22:30:11 +0100

    creating an array for possible locations for the player to go.

--- src/Location.java
+++ src/Location.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+
 public class Location {
+    private String[] posibleLocations;
+
+    public Location(String[] posibleLocations) {
+        this.posibleLocations = posibleLocations;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "posibleLocations=" + Arrays.toString(posibleLocations) +
+                '}';
+    }
 }
