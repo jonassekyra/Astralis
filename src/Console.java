04bd95d1f4d2dc8f59a2843e6f1882ccbbe4cb23
jonas <jonassekyra@gmail.com>
2025-02-26 20:35:17 +0100

    Adding hashmap, scanner and inicialization method.

--- src/Console.java
+++ src/Console.java
@@ -1,2 +1,17 @@
+import Command.*;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Console {
+    private boolean exit = false;
+    private HashMap<String, Command> commands = new HashMap<String, Command>();
+
+    public void inicialization(){
+        commands.put("go to", new GoTo());
+    }
+
+    Scanner sc = new Scanner(System.in);
+
+
 }
