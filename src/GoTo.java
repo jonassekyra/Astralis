8589c0a5775ba511000c6b81b6b5f3a55a626f76
jonas <jonassekyra@gmail.com>
2025-02-26 18:41:34 +0100

    implementing methods execute and exit

--- src/GoTo.java
+++ src/GoTo.java
@@ -1,2 +1,12 @@
-public class GoTo {
+public class GoTo implements Comand{
+
+    @Override
+    public String execute() {
+        return "";
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
 }
