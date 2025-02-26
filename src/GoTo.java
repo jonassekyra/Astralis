edffc5556eaae011b3963c6f3d11155bcf2bdbe7
jonas <jonassekyra@gmail.com>
2025-02-26 20:18:49 +0100

    adding functionality to the execute method.

--- src/GoTo.java
+++ src/GoTo.java
@@ -1,8 +1,30 @@
-public class GoTo implements Comand{
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GoTo implements Command {
+    Scanner sc = new Scanner(System.in);
+    WorldMap worldMap = new WorldMap();
+    private String currentLocation;
+
 
     @Override
     public String execute() {
-        return "";
+        currentLocation = worldMap.getCurrentPosition();
+        System.out.println("kam chcete jit?");
+        String input = sc.nextLine();
+        if (worldMap.locations.containsKey(input)) {
+            if (Arrays.asList(worldMap.locations.get(currentLocation).getPosibleLocations()).contains(input)) {
+                worldMap.setCurrentPosition(input);
+
+        }else {
+                System.out.println("do teto lokace se odsud nedostanete");
+            }
+
+    }else {
+            System.out.println("lokace nexistuje");
+        }
+
+        return "jste v: " + worldMap.getCurrentPosition() ;
     }
 
     @Override
