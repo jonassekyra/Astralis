b47b767177fc39a242284ec3abc159ae4045429c
jonas <jonassekyra@gmail.com>
2025-03-05 21:34:53 +0100

    Removing methods.

--- src/Player/Backpack.java
+++ src/Player/Backpack.java
@@ -4,19 +4,9 @@ import java.util.ArrayList;
 
 public class Backpack {
     private ArrayList<Item> myItems;
-    private ArrayList<Item> allItems;
 
     public Backpack() {
         myItems = new ArrayList<Item>();
-        allItems = new ArrayList<Item>();
-    }
-    public boolean loadItems() {
-        return true;
-    }
-
-    public boolean removeItem(String Item) {
-        return true;
-
     }
 
     public boolean addToMyItem(String Item) {
