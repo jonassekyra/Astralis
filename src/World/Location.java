9328d8a7910daf24bcfc205e079908b5bdf12337
jonas <jonassekyra@gmail.com>
2025-03-06 11:00:29 +0100

    Adding toString

--- src/World/Location.java
+++ src/World/Location.java
@@ -1,12 +1,21 @@
 package World;
 
+import Player.Item;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Location {
     private String name;
     private String[] posibleLocations;
+    private ArrayList<Item> items;
+    private HashSet<NPC> NPCS;
 
-    public Location(String name,String[] posibleLocations) {
+    public Location(String name, String[] posibleLocations, ArrayList<Item> items) {
         this.name = name;
         this.posibleLocations = posibleLocations;
+        this.items = items;
     }
 
     public String[] getPosibleLocations() {
@@ -16,4 +25,12 @@ public class Location {
     public Location() {
     }
 
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", posibleLocations=" + Arrays.toString(posibleLocations) +
+                ", items=" + items+
+                '}';
+    }
 }
