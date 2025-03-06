ac846c3430cf432d4f3b95feb08e13c09f0a6821
jonas <jonassekyra@gmail.com>
2025-03-06 10:59:42 +0100

    Adding toString

--- src/Player/Item.java
+++ src/Player/Item.java
@@ -3,7 +3,14 @@ package Player;
 public class Item {
     private String name;
 
-    public Item(String name, String description) {
+    public Item(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
