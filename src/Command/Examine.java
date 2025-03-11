d6af4a21dbd4c2ab1f3a261d3a5d9660bf654ff1
jonas <jonassekyra@gmail.com>
2025-03-11 23:02:11 +0100

    Adding a constructor and execute functionality.

--- src/Command/Examine.java
+++ src/Command/Examine.java
@@ -1,16 +1,28 @@
 package Command;
 import Player.Item;
 import World.NPC;
+import World.WorldMap;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Examine implements Command{
+    private WorldMap worldMap;
 
-
+    public Examine(WorldMap worldMap) {
+        this.worldMap = worldMap;
+    }
 
     @Override
     public String execute() {
-        return "";
+        if (!worldMap.locations.get(worldMap.getCurrentPosition()).isExamined()){
+            worldMap.locations.get(worldMap.getCurrentPosition()).setExamined(true);
+            return "you found: " + worldMap.locations.get(worldMap.getCurrentPosition()).getItems();
+
+        }else {
+            return "allready examined";
+        }
+
     }
 
     @Override
