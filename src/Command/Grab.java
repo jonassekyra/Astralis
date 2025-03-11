23cbe143b666bd46400befe6ac1d09e818dac4d8
jonas <jonassekyra@gmail.com>
2025-03-11 21:15:25 +0100

    adding player as an attribute and adding functionality to execute

--- src/Command/Grab.java
+++ src/Command/Grab.java
@@ -1,17 +1,29 @@
 package Command;
-
+import Player.Item;
+import Player.Player;
 import World.WorldMap;
+
+import java.util.Scanner;
 
 public class Grab implements Command {
     private WorldMap worldMap;
+    private Player p;
+    Scanner sc = new Scanner(System.in);
 
-    public Grab(WorldMap worldMap) {
+    public Grab(WorldMap worldMap, Player p) {
         this.worldMap = worldMap;
+        this.p = p;
     }
 
     @Override
     public String execute() {
-        return "";
+        Item input = new Item(sc.nextLine());
+        if (worldMap.locations.get(worldMap.getCurrentPosition()).getItems().contains(input)) {
+            p.addItem(input);
+            worldMap.locations.get(worldMap.getCurrentPosition()).getItems().remove(input);
+
+        }
+        return " items: "+ p.getItems().toString();
     }
 
     @Override
