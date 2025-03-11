593628cca04ddb7fb585769137f38b91f30ac63e
jonas <jonassekyra@gmail.com>
2025-03-11 21:13:06 +0100

    Adding equals and hash code to Item

--- src/Player/Item.java
+++ src/Player/Item.java
@@ -1,5 +1,7 @@
 package Player;
 
+import java.util.Objects;
+
 public class Item {
     private String name;
 
@@ -7,10 +9,27 @@ public class Item {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return "Item{" +
                 "name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
