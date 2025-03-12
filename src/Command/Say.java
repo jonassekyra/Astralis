ee645f9bb2d7efd4fb3185f0be67cb77805b080c
jonas <jonassekyra@gmail.com>
2025-03-12 18:15:32 +0100

    Completing execute method.

--- src/Command/Say.java
+++ src/Command/Say.java
@@ -1,13 +1,31 @@
 package Command;
 
+import World.WorldMap;
+
+import java.util.Scanner;
+
 public class Say implements Command {
+    Scanner sc = new Scanner(System.in);
+    private final WorldMap worldMap;
     @Override
     public String execute() {
-        return "";
+        if(worldMap.locations.get(worldMap.getCurrentPosition()).equals("zkamenely les")) {
+            System.out.println("Co chcete rict?");
+            String input = sc.nextLine().trim().toLowerCase();
+            if(input.equals("strom")) {
+                return "spravne";
+            }
+
+        }return "Ted dava opravdu velky smysl neco rikat, ze jo .-. ";
+
     }
 
     @Override
     public boolean exit() {
         return false;
+    }
+
+    public Say(WorldMap worldMap) {
+        this.worldMap = worldMap;
     }
 }
