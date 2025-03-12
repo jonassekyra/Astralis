20a4e525afac14b3ebfc32bd674156b5a79f5f9e
jonas <jonassekyra@gmail.com>
2025-03-12 14:54:09 +0100

    Adding unlocked condition, toString and constructor

--- src/World/Task.java
+++ src/World/Task.java
@@ -2,5 +2,16 @@ package World;
 
 public class Task {
     private String text;
-    private boolean completed;
+    private boolean completed = false ;
+    private String unlockedCondition;
+
+    public Task(String text, String unlockedCondition) {
+        this.text = text;
+        this.unlockedCondition = unlockedCondition;
+    }
+
+    @Override
+    public String toString() {
+        return "Task: " + text;
+    }
 }
