96f75b1c8f9b5bf301b16a0ac758be957e5b2109
jonas <jonassekyra@gmail.com>
2025-03-12 18:16:06 +0100

    Completing execute method, adding constructor.

--- src/Command/SpeakTo.java
+++ src/Command/SpeakTo.java
@@ -1,9 +1,40 @@
 package Command;
 
+import World.NPC;
+import World.WorldMap;
+
+import java.util.Scanner;
+
+
+
+
 public class SpeakTo implements Command {
+    public SpeakTo(WorldMap worldMap) {
+        this.worldMap = worldMap;
+    }
+
+    private final WorldMap worldMap;
     @Override
     public String execute() {
-        return "";
+        Scanner sc = new Scanner(System.in);
+        if (worldMap.locations.get(worldMap.getCurrentPosition()).isExamined()){
+            if (!worldMap.locations.get(worldMap.getCurrentPosition()).getNPCS().isEmpty()){
+                System.out.println("S kym by jsi chtel mluvit");
+                String input = sc.nextLine().trim().toLowerCase();
+                for (NPC npc : worldMap.locations.get(worldMap.getCurrentPosition()).getNPCS()) {
+                    if (input.equals(npc.getName())) {
+                        String dialog = npc.getDialogs().get(npc.getDialogCount());
+                        npc.setDialogCount(npc.getDialogCount() + 1);
+                        return dialog;
+                    }
+                }
+        }else {
+             return "Neni tu nikdo s kym jsi mohl mluvit";
+            }
+
+
+        }return "Tuhle lokaci jste jeste neprozkoumali.";
+
     }
 
     @Override
