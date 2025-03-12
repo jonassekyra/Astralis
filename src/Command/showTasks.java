594bdd762a61efe2c06e2de44fd91ac94d2b1272
jonas <jonassekyra@gmail.com>
2025-03-12 13:11:55 +0100

    Completing execute method.

--- src/Command/showTasks.java
+++ src/Command/showTasks.java
@@ -1,14 +1,21 @@
 package Command;
 
+import Player.Player;
+
 public class showTasks implements Command {
+    private Player player;
 
     @Override
     public String execute() {
-        return "";
+        return "vase ukoly: " + player.getAccesibleTasks();
     }
 
     @Override
     public boolean exit() {
         return false;
+    }
+
+    public showTasks(Player player) {
+        this.player = player;
     }
 }
