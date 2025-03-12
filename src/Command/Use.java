a57ece46200bedbe31b2e3f5c0642da2abad48f6
jonas <jonassekyra@gmail.com>
2025-03-12 18:16:18 +0100

    Completing execute method, adding constructor.

--- src/Command/Use.java
+++ src/Command/Use.java
@@ -1,14 +1,34 @@
 package Command;
+import Player.Player;
+import Player.Item;
+import java.util.Scanner;
 
 public class Use implements Command {
+    private final Player player;
+    Scanner sc = new Scanner(System.in);
 
     @Override
     public String execute() {
-        return "";
+        if (!player.getItems().isEmpty()) {
+            System.out.println("Jaky item chcete pouzit?");
+            String input = sc.nextLine().toLowerCase().trim();
+            for (Item item : player.getItems()) {
+                if(item.getName().equals(input)) {
+                    player.getItems().remove(item);
+                }
+            }
+        }else {
+            return "Nemate zadny item na pouziti";
+        }
+        return "pouzito uspesne";
     }
 
     @Override
     public boolean exit() {
         return false;
+    }
+
+    public Use(Player player) {
+        this.player = player;
     }
 }
