960d9ebe535a1cbcdf50b35c042bda9f15c8333b
jonas <jonassekyra@gmail.com>
2025-03-18 23:38:25 +0100

    Redoing execute method to include task checks, not finished yet.

--- src/Command/Use.java
+++ src/Command/Use.java
@@ -1,26 +1,85 @@
 package Command;
+
 import Player.Player;
 import Player.Item;
+import World.Task;
+import World.WorldMap;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Use implements Command {
     private final Player player;
+    private final WorldMap worldMap;
     Scanner sc = new Scanner(System.in);
 
     @Override
     public String execute() {
-        if (!player.getItems().isEmpty()) {
-            System.out.println("Jaky item chcete pouzit?");
-            String input = sc.nextLine().toLowerCase().trim();
-            for (Item item : player.getItems()) {
-                if(item.getName().equals(input)) {
-                    player.getItems().remove(item);
-                }
-            }
-        }else {
+        if (player.getItems().isEmpty()) {
             return "Nemate zadny item na pouziti";
         }
-        return "pouzito uspesne";
+        System.out.println("Jaky item chcete pouzit?");
+        String input = sc.nextLine().toLowerCase().trim();
+
+        Iterator<Item> it = player.getItems().iterator();
+        boolean itemUsed = false;
+        Item itemToRemove = null;
+
+         ArrayList<Task> taskToAdd = new ArrayList<Task>();
+         ArrayList<Task> tasksToRemove = new ArrayList<>();
+
+        while (it.hasNext()) {
+            Item item = it.next();
+
+            if (item.getName().equalsIgnoreCase(input)) {
+                if (!player.canUseItem(item, worldMap.getCurrentPosition())) {
+                    return "tento item zde nelze pouzit";
+                }
+                int count = 0;
+                if (player.getAllTasks() != null) {
+
+
+
+                    for (Task task : player.getAllTasks()) {
+
+                        if (task.getUnlockedCondition() != null && task.getUnlockedCondition().equals(item.getName()) && player.getAccesibleTasks() != null && !player.getAccesibleTasks().contains(task)) {
+                            taskToAdd.add(task);
+                            tasksToRemove.add(task);
+                            count++;
+                            System.out.println("novy ukol: " + task.getText());
+
+
+                        }
+                        // task complete
+                        if (task.getRequiredLocation() != null && task.getRequiredLocation().equals(worldMap.getCurrentPosition()) && task.getRequiredItemOrInteraction() != null && task.getRequiredItemOrInteraction().equals(item.getName())) {
+                            player.compleateTask(task);
+                            count++;
+                            System.out.println("ukol splnen");
+                        }
+
+                    }
+
+                }
+                if (count > 0) {
+                    itemUsed = true;
+                    itemToRemove = item;
+                    return "pouzito";
+                } else {
+                    return "tento item zde nelze pouzit";
+                }
+            }
+            player.getAccesibleTasks().addAll(taskToAdd);
+            player.getAllTasks().removeAll(tasksToRemove);
+
+            if (itemUsed && itemToRemove != null) {
+                it.remove();
+            }
+
+        }
+        return "tento item nelze pouzit";
+
+
     }
 
     @Override
@@ -28,7 +87,8 @@ public class Use implements Command {
         return false;
     }
 
-    public Use(Player player) {
+    public Use(Player player, WorldMap worldMap) {
         this.player = player;
+        this.worldMap = worldMap;
     }
 }
