287f8532353ca5c8d6f82da1a19547e609d045b6
jonas <jonassekyra@gmail.com>
2025-03-19 21:42:16 +0100

    Adding or fixing task checks.

--- src/Command/GoTo.java
+++ src/Command/GoTo.java
@@ -1,25 +1,47 @@
 package Command;
+import Player.Player;
+import World.Task;
 import World.WorldMap;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class GoTo implements Command {
     Scanner sc = new Scanner(System.in);
     private final WorldMap worldMap;
-    private String currentLocation;
+    private final Player player;
 
-    public GoTo(WorldMap worldMap) {
+    public GoTo(WorldMap worldMap, Player player) {
         this.worldMap = worldMap;
+        this.player = player;
     }
 
     @Override
     public String execute() {
-        currentLocation = worldMap.getCurrentPosition();
         System.out.println("kam chcete jit?");
         String input = sc.nextLine();
         if (worldMap.locations.containsKey(input)) {
-            if (Arrays.asList(worldMap.locations.get(currentLocation).getPosibleLocations()).contains(input)) {
+            if (Arrays.asList(worldMap.locations.get(worldMap.getCurrentPosition()).getPosibleLocations()).contains(input)) {
                 worldMap.setCurrentPosition(input);
+                //Iterator for unlocking new tasks
+                Iterator<Task> taksIterator = player.getAllTasks().iterator();
+                while (taksIterator.hasNext()) {
+                    Task task = taksIterator.next();
+                    if (task.getUnlockedCondition().equals(input)) {
+                        player.getAccesibleTasks().add(task);
+                        System.out.println("novy ukol: " + task.getText());
+                    }
+                }
+                Iterator<Task> completeIterator = player.getAllTasks().iterator();
+                while (completeIterator.hasNext()) {
+                    Task task = completeIterator.next();
+                    if (task.getRequiredItemOrInteraction().equals(input) && task.getRequiredLocation().equals(worldMap.getCurrentPosition())) {
+                        completeIterator.remove();
+                        player.compleateTask(task);
+                        System.out.println("ukol splnen");
+                    }
+                }
+
         }else {
                 System.out.println("do teto lokace se odsud nedostanete");
             }
--- src/Command/SpeakTo.java
+++ src/Command/SpeakTo.java
@@ -1,19 +1,24 @@
 package Command;
 
+import Player.Player;
 import World.NPC;
+import World.Task;
 import World.WorldMap;
 
+import java.util.Iterator;
 import java.util.Scanner;
 
 
 
 
 public class SpeakTo implements Command {
-    public SpeakTo(WorldMap worldMap) {
+    public SpeakTo(WorldMap worldMap, Player player) {
         this.worldMap = worldMap;
+        this.player = player;
     }
 
     private final WorldMap worldMap;
+    private final Player player;
     @Override
     public String execute() {
         Scanner sc = new Scanner(System.in);
@@ -21,6 +26,24 @@ public class SpeakTo implements Command {
             if (!worldMap.locations.get(worldMap.getCurrentPosition()).getNPCS().isEmpty()){
                 System.out.println("S kym by jsi chtel mluvit");
                 String input = sc.nextLine().trim().toLowerCase();
+                //Iterator for unlocking new tasks
+                Iterator<Task> taksIterator = player.getAllTasks().iterator();
+                while (taksIterator.hasNext()) {
+                    Task task = taksIterator.next();
+                    if (task.getUnlockedCondition().equals(input)) {
+                        player.getAccesibleTasks().add(task);
+                        System.out.println("novy ukol: " + task.getText());
+                    }
+                }
+                Iterator<Task> completeIterator = player.getAllTasks().iterator();
+                while (completeIterator.hasNext()) {
+                    Task task = completeIterator.next();
+                    if (task.getRequiredItemOrInteraction().equals(input) && task.getRequiredLocation().equals(worldMap.getCurrentPosition())) {
+                        completeIterator.remove();
+                        player.compleateTask(task);
+                        System.out.println("ukol splnen");
+                    }
+                }
                 for (NPC npc : worldMap.locations.get(worldMap.getCurrentPosition()).getNPCS()) {
                     if (input.equals(npc.getName())) {
                         String dialog = npc.getDialogs().get(npc.getDialogCount());
