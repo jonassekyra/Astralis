63552fd8d98ad9cae193e4008a2ae99955bb2a64
jonas <jonassekyra@gmail.com>
2025-03-19 21:44:22 +0100

    Removing addAccesibleTask method and adding canUseItem method

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,13 +1,12 @@
 package Player;
 import World.Task;
 import World.WorldMap;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Objects;
+
 
 public class Player {
     private ArrayList<Item> items;
@@ -27,11 +26,12 @@ public class Player {
     public Player() {
         this. items = new ArrayList<Item>();
         this.accesibleTasks = new ArrayList<>();
+        this.completedTasks = new ArrayList<>();
     }
 
-    public void compleateTask() {}
-    public void addAccesibleTask() {
-
+    public void compleateTask(Task task) {
+        completedTasks.add(task);
+        accesibleTasks.remove(task);
     }
     public void loadTasks() {
         allTasks = new ArrayList<>();
@@ -39,12 +39,13 @@ public class Player {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                Task task = new Task(parts[0],parts[1]);
+                Task task = new Task(parts[0],parts[1],parts[2],parts[3]);
                 if (!parts[1].equals("start")) {
                     allTasks.add(task);
 
                 }else{
                     accesibleTasks.add(task);
+                    allTasks.add(task);
                 }
 
             }
@@ -59,4 +60,33 @@ public class Player {
     public ArrayList<Task> getAccesibleTasks() {
         return accesibleTasks;
     }
+
+
+    public ArrayList<Task> getAllTasks() {
+        return allTasks;
+    }
+
+    public ArrayList<Task> getCompletedTasks() {
+        return completedTasks;
+    }
+
+    public boolean canUseItem(Item item,String currentLocation) {
+        for (Task task : allTasks) {
+            if ((task.getRequiredLocation() != null && task.getRequiredLocation().equals(currentLocation) && task.getRequiredItemOrInteraction() != null && task.getRequiredItemOrInteraction().equals(item.getName()))) {
+                return true;
+            }
+            if (task.getUnlockedCondition() != null && task.getUnlockedCondition().equals(item.getName()) && getAccesibleTasks() != null && !getAccesibleTasks().contains(task)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+
+
+
+
+
 }
